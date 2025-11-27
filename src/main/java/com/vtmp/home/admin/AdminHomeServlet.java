df6d3c3355018649df8fd7e0f8b7c625309e37f9
joelmathewthomas <proxy0@tutamail.com>
2025-11-28 01:12:21 +0530

    AdminHomeServlet

---
+++ src/main/java/com/vtmp/home/admin/AdminHomeServlet.java
@@ -0,0 +1,37 @@
+package com.vtmp.home.admin;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class AdminHomeServlet
+ */
+@WebServlet("/admin")
+public class AdminHomeServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		request.getRequestDispatcher("/WEB-INF/views/admin/adminHome.jsp").forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.setStatus(HttpServletResponse.SC_METHOD_NOT_ALLOWED);
+		response.setContentType("text/plain");
+		response.getWriter().write("METHOD_NOT_ALLOWED");
+	}
+
+}
