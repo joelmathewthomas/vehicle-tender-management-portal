12868908557ae2a37e8575318877e46ad87adc14
joelmathewthomas <proxy0@tutamail.com>
2025-12-01 18:54:31 +0530

    Rename files

--- src/main/java/com/vtmp/home/admin/owner/OwnerTableServlet.java
+++ src/main/java/com/vtmp/home/admin/owner/OwnerTableServlet.java
@@ -14,7 +14,7 @@ import javax.servlet.http.HttpServletResponse;
  * Servlet implementation class OwnerTable
  */
 @WebServlet("/admin/owner")
-public class OwnerTable extends HttpServlet {
+public class OwnerTableServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	/**
